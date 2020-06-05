64b90050ba38abde2e5d1c5533dca4fb988bdb9e
maslowind <bart1885@gmail.com>
2020-06-05 14:40:53 +0300

    bot v 1.1.0

--- BotFunction/src/index.js
+++ BotFunction/src/index.js
@@ -12,37 +12,50 @@ var ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
 
 let TABLE_NAME = 'BotStorage-master';
 
+let setBD = false, setDate = false, deleteItem = false;
+let userData = null;
+
 var ID = function () {
     return Math.random().toString(9).substr(2, 15);
 };
 
-function checkAddBDMessage(message) {
-    let message_data = message.text.replace(/\s+/g, ' ').split(" ");
-    if (message_data.length < 3) return null;
-    let username = "";
-    for (let i = 0; i < message_data.length - 2; i++) {
-        username += message_data[1 + i];
-        if (i != message_data.length - 3)
-            username += " ";
-    }
-    let bd_dateParams = message_data[message_data.length - 1].split(".");
+
+
+function checkAddBDdateMessage(user_id, username, date) {
+
+    let bd_dateParams = date.split(".");
     if (bd_dateParams.length !== 2) return null;
     if (!(bd_dateParams[0] >= 1 && bd_dateParams[0] <= 31 && bd_dateParams[1] >= 1 && bd_dateParams[1] <= 12)) return null;
+    /*if (!(bd_dateParams[0] >= 1 && bd_dateParams[0] <= 29 && bd_dateParams[1] == 2)) return null;
+    if (!(bd_dateParams[0] >= 1 && bd_dateParams[0] <= 30 && bd_dateParams[1] == 4)) return null;
+    if (!(bd_dateParams[0] >= 1 && bd_dateParams[0] <= 30 && bd_dateParams[1] == 6)) return null;
+    if (!(bd_dateParams[0] >= 1 && bd_dateParams[0] <= 30 && bd_dateParams[1] == 9)) return null;
+    if (!(bd_dateParams[0] >= 1 && bd_dateParams[0] <= 30 && bd_dateParams[1] == 11)) return null;*/
     let bd_date = bd_dateParams[0] + "." + bd_dateParams[1];
-    createNewData(message.chat.id, username, bd_date);
+    createNewData(user_id, username, bd_date);
     return bd_date;
 }
 
-async function checkDelBDMessage(message) {
+function checkAddBDusernameMessage(message_text) {
+    let message_data = message_text.replace(/\s+/g, ' ').split(" ");
     let username = "";
-    let message_data = message.text.replace(/\s+/g, ' ').split(" ");
-    if (message_data.length < 2) return false;
-    for (let i = 0; i < message_data.length - 1; i++) {
-        username += message_data[1 + i];
-        if (i != message_data.length - 2)
+    for (let i = 0; i < message_data.length; i++) {
+        username += message_data[i];
+        if (i != message_data.length - 1)
             username += " ";
     }
-    await delUserData(message.chat.id, username);
+    return username;
+}
+async function checkDelBDMessage(user_id, message_text) {
+    let username = "";
+    let message_data = message_text.replace(/\s+/g, ' ').split(" ");
+    for (let i = 0; i < message_data.length; i++) {
+        username += message_data[i];
+        if (i != message_data.length - 1)
+            username += " ";
+    }
+    let result = await delUserData(user_id, username);
+    if(result.Items.length===0) return false
     return true;
 }
 
@@ -97,7 +110,7 @@ async function delUserData(user_id, username) {
     };
 
 
-    await ddb.query(params, function (err, data) {
+   return await ddb.query(params, function (err, data) {
         if (err) {
             console.log("Error", err);
         } else {
@@ -145,30 +158,60 @@ async function sendMessage(url, chat_id, reply) {
 }
 let telegram_url = "https://api.telegram.org/bot" + process.env.TOKEN + "/sendMessage";
 
+function stopProcess() {
+    setBD = false, setDate = false, deleteItem = false;
+    userData = null;
+}
+
 exports.handler = async (event) => {
     const message = event.message;
     console.log(message);
     if (message.text.toLowerCase().indexOf("/start") != -1) {
-        let infoMessage = `Привет! Я бот, который напоминает о ДР твоих друзей. Вот список моих команд:\nДобавить друга в список напоминания: "/setbirthday @username DD.MM"
-Посмотреть список друзей и их ДР: "/getbdlist"\nУдалить друга из списка:"/delete @username"`
+        stopProcess();
+        let infoMessage = `Привет! Я бот, который напоминает о ДР твоих друзей. Вот список моих команд:\nДобавить друга в список напоминания: "/setbirthday"
+Посмотреть список друзей и их ДР: "/getbdlist"\nУдалить друга из списка:"/delete".`
         await sendMessage(telegram_url, message.chat.id, infoMessage);
 
     } else if (message.text.toLowerCase().indexOf("/setbirthday") != -1) {
-        let result = checkAddBDMessage(message);
-        if (result !== null)
-            await sendMessage(telegram_url, message.chat.id, "Вы успешно добавили друга в список");
-        else
-            await sendMessage(telegram_url, message.chat.id, `Дата должна быть в формате "/setbirthday @username DD.MM"`);
+        stopProcess();
+        setBD = true;
+        await sendMessage(telegram_url, message.chat.id, `Введите имя юзера, которого хотите добавить. Или /stop, чтоб остановить процесс добавления.`);
+        
     } else if (message.text.toLowerCase().indexOf("/getbdlist") != -1) {
+        stopProcess();
         let message_bd_list = await getUserList(message);
         await sendMessage(telegram_url, message.chat.id, message_bd_list);
-
     } else if (message.text.toLowerCase().indexOf("/delete") != -1) {
-        if (await checkDelBDMessage(message)) {
-            await sendMessage(telegram_url, message.chat.id, "Удаление было совершено.\n" );
+        stopProcess();
+        deleteItem = true;
+        await sendMessage(telegram_url, message.chat.id, `Введите имя юзера, которого хотите удалить. Или /stop, чтоб остановить процесс удаления.`);
+        
+    } else if (message.text.toLowerCase().indexOf("/stop") != -1) {
+        stopProcess();
+        await sendMessage(telegram_url, message.chat.id, "Все процессы остановлены.");
+    } else if (message.text && setBD) {
+        userData = checkAddBDusernameMessage(message.text);
+        setBD = false; setDate = true;
+        await sendMessage(telegram_url, message.chat.id, `Введите дату рождения в формате "DD.MM". Или /stop, чтоб остановить процесс добавления.`);
+    } else if (message.text && setDate) {
+        let result = checkAddBDdateMessage(message.chat.id, userData, message.text);
+        if (result !== null){
+            await sendMessage(telegram_url, message.chat.id, "Вы успешно добавили друга в список.");
+            setDate = false;   
+            userData = null;             
         }
-        else await sendMessage(telegram_url, message.chat.id, `Удаление должно быть в формате "/delete @username"`);
-    } else await sendMessage(telegram_url, message.chat.id, `${message.from.first_name}... Ти не то робиш`);
+        else
+            await sendMessage(telegram_url, message.chat.id, `Дата рождения должна быть в формате "DD.MM".`);
+    } else if (message.text && deleteItem) {
+        deleteItem = false;
+        if (await checkDelBDMessage(message.chat.id, message.text)) {
+            await sendMessage(telegram_url, message.chat.id, "Удаление было совершено.");
+            
+        } else await sendMessage(telegram_url, message.chat.id, "Данный юзер в списке не найден.");
+    } else {
+        stopProcess();
+        await sendMessage(telegram_url, message.chat.id, `${message.from.first_name}... Ти не то робиш.`);
+    }
 
     const response = {
         statusCode: 200,
